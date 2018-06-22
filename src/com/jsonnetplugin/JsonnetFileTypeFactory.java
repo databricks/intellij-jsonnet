8cb43ff998159d077e42ad693824ac190e3610fc
Li Haoyi <haoyi.li@databricks.com>
2018-06-22 13:15:01 -0700

    also handle .jsonnet.TEMPLATE files

--- src/com/jsonnetplugin/JsonnetFileTypeFactory.java
+++ src/com/jsonnetplugin/JsonnetFileTypeFactory.java
@@ -6,6 +6,34 @@ import org.jetbrains.annotations.NotNull;
 public class JsonnetFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
-        fileTypeConsumer.consume(JsonnetFileType.INSTANCE);
+        fileTypeConsumer.consume(
+                JsonnetFileType.INSTANCE,
+                new FileNameMatcher(){
+
+                    @Override
+                    public boolean accept(@NotNull String fileName) {
+                        return fileName.endsWith(".jsonnet");
+                    }
+
+                    @NotNull
+                    @Override
+                    public String getPresentableString() {
+                        return ".jsonnet";
+                    }
+                },
+                new FileNameMatcher(){
+
+                    @Override
+                    public boolean accept(@NotNull String fileName) {
+                        return fileName.endsWith(".jsonnet.TEMPLATE");
+                    }
+
+                    @NotNull
+                    @Override
+                    public String getPresentableString() {
+                        return ".jsonnet.TEMPLATE";
+                    }
+                }
+        );
     }
 }
