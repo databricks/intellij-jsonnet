b3ec2d6524fa305e3fecaab7ccb13d9a558059ae
Andrew Chen <andrewmnchen@gmail.com>
2018-06-21 22:46:52 -0700

    add file completion

---
+++ src/com/jsonnetplugin/Constants.java
@@ -0,0 +1,5 @@
+package com.jsonnetplugin;
+
+public class Constants {
+    public static String INTELLIJ_RULES = "IntellijIdeaRulezzz ";
+}
--- src/com/jsonnetplugin/JsonnetCompletionContributor.java
+++ src/com/jsonnetplugin/JsonnetCompletionContributor.java
@@ -1,30 +1,101 @@
 package com.jsonnetplugin;
 
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.lookup.LookupElement;
 import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import com.intellij.util.ProcessingContext;
+import com.jsonnetplugin.psi.JsonnetFile;
+import com.jsonnetplugin.psi.JsonnetImportop;
 import com.jsonnetplugin.psi.JsonnetTypes;
 import org.jetbrains.annotations.NotNull;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonnetCompletionContributor extends CompletionContributor {
     public JsonnetCompletionContributor() {
-        System.out.println("here");
+        extend(CompletionType.BASIC,
+                PlatformPatterns.psiElement(JsonnetTypes.IDENTIFIER).withLanguage(JsonnetLanguage.INSTANCE),
+                new CompletionProvider<CompletionParameters>() {
+                    public void addCompletions(@NotNull CompletionParameters parameters,
+                                               ProcessingContext context,
+                                               @NotNull CompletionResultSet resultSet) {
+                        resultSet.addElement(LookupElementBuilder.create("null"));
+                        resultSet.addElement(LookupElementBuilder.create("true"));
+                        resultSet.addElement(LookupElementBuilder.create("false"));
+                        resultSet.addElement(LookupElementBuilder.create("local"));
+                    }
+                }
+        );
         extend(CompletionType.BASIC,
                 PlatformPatterns.psiElement(JsonnetTypes.DOUBLE_QUOTED_STRING).withLanguage(JsonnetLanguage.INSTANCE),
                 new CompletionProvider<CompletionParameters>() {
                     public void addCompletions(@NotNull CompletionParameters parameters,
                                                ProcessingContext context,
                                                @NotNull CompletionResultSet resultSet) {
-                        System.out.println("lol");
-                        resultSet.addElement(LookupElementBuilder.create("a null"));
-                        resultSet.addElement(LookupElementBuilder.create("null"));
-                        resultSet.addElement(LookupElementBuilder.create(JsonnetTypes.TRUE.toString()));
-                        resultSet.addElement(LookupElementBuilder.create(JsonnetTypes.FALSE.toString()));
-                        resultSet.addElement(LookupElementBuilder.create(JsonnetTypes.LOCAL.toString()));
-                        resultSet.addElement(LookupElementBuilder.create(JsonnetTypes.LOCAL.toString()));
+                        if (checkIfImport(parameters.getPosition())) {
+                            String text = parameters.getPosition().getText();
+                            System.out.println(text);
+                            addFileCompletions(parameters.getOriginalFile(), text, resultSet);
+                        }
                     }
                 }
         );
+    }
+
+    private static boolean checkIfImport(PsiElement position) {
+        return position.getPrevSibling() != null &&
+          position.getPrevSibling().getPrevSibling().getNode().getElementType().equals(JsonnetTypes.IMPORT);
+    }
+
+    private static void addFileCompletions(PsiFile file, String current, CompletionResultSet set) {
+        // current always begins with a "
+        String cleanedCurrent = current.substring(1);
+        if (cleanedCurrent.endsWith("\"")) {
+           cleanedCurrent = cleanedCurrent.substring(0, cleanedCurrent.length() - 1);
+        }
+
+        if (!cleanedCurrent.endsWith(Constants.INTELLIJ_RULES)) {
+            return;
+        }
+        Path currentPath = Paths.get(file.getContainingDirectory().getVirtualFile().getPath());
+        String stripped = cleanedCurrent.replace(Constants.INTELLIJ_RULES, "");
+        Path strippedPath = Paths.get(stripped);
+        int strippedPathCount = strippedPath.getNameCount();
+
+        File prefixFile;
+        String input;
+        if (stripped.endsWith("/")) {
+            prefixFile = currentPath.resolve(Paths.get(stripped)).toFile();
+            input = "";
+        } else if (strippedPathCount == 1){
+            prefixFile = currentPath.toFile();
+            input = stripped;
+        } else {
+            prefixFile = currentPath.resolve(strippedPath.subpath(0, strippedPathCount - 1)).toFile();
+            input = strippedPath.subpath(strippedPathCount-1, strippedPathCount).toString();
+        }
+
+        CompletionResultSet replaceSet = set.withPrefixMatcher(stripped);
+        if (prefixFile.isDirectory()) {
+            File[] files = prefixFile.listFiles(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    return name.startsWith(input);
+                }
+            });
+            for (File f: files) {
+               String result = stripped + f.getName().substring(input.length());
+               replaceSet.addElement(LookupElementBuilder.create(result));
+            }
+        }
     }
 }
