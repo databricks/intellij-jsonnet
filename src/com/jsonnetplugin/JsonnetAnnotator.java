1e4bb094240695fc5790bbd76f48c260e21eb527
Li Haoyi <haoyi.li@databricks.com>
2019-03-07 14:28:03 -0800

    highlight fields differently from locals

---
+++ src/com/jsonnetplugin/JsonnetAnnotator.java
@@ -0,0 +1,26 @@
+package com.jsonnetplugin;
+
+import com.intellij.lang.annotation.AnnotationHolder;
+import com.intellij.lang.annotation.Annotator;
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
+import com.intellij.openapi.editor.colors.TextAttributesKey;
+import com.intellij.psi.PsiElement;
+import com.jsonnetplugin.psi.JsonnetFieldname;
+import com.jsonnetplugin.psi.JsonnetIdentifier0;
+import com.jsonnetplugin.psi.JsonnetSelect;
+import org.jetbrains.annotations.NotNull;
+
+import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
+
+public class JsonnetAnnotator implements Annotator {
+    TextAttributesKey MEMBER = createTextAttributesKey("JSONNET_MEMBER", DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+    @Override
+    public void annotate(@NotNull PsiElement psiElement, @NotNull AnnotationHolder annotationHolder) {
+        if (psiElement instanceof JsonnetIdentifier0 && psiElement.getParent() instanceof JsonnetFieldname){
+            annotationHolder.createInfoAnnotation(psiElement, "").setTextAttributes(MEMBER);
+        }
+        if (psiElement instanceof JsonnetIdentifier0 && psiElement.getParent() instanceof JsonnetSelect){
+            annotationHolder.createInfoAnnotation(psiElement, "").setTextAttributes(MEMBER);
+        }
+    }
+}
