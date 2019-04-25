0a9ed9b7c249c9f3ec24d9af6f2e86054a1c06cf
Santiago Castro <sacastro@umich.edu>
2019-04-25 18:20:13 -0400

    Support libsonnet file extensions

--- src/main/java/com/jsonnetplugin/JsonnetFileTypeFactory.java
+++ src/main/java/com/jsonnetplugin/JsonnetFileTypeFactory.java
@@ -6,34 +6,6 @@ import org.jetbrains.annotations.NotNull;
 public class JsonnetFileTypeFactory extends FileTypeFactory {
     @Override
     public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
-        fileTypeConsumer.consume(
-                JsonnetFileType.INSTANCE,
-                new FileNameMatcher(){
-
-                    @Override
-                    public boolean accept(@NotNull String fileName) {
-                        return fileName.endsWith(".jsonnet");
-                    }
-
-                    @NotNull
-                    @Override
-                    public String getPresentableString() {
-                        return ".jsonnet";
-                    }
-                },
-                new FileNameMatcher(){
-
-                    @Override
-                    public boolean accept(@NotNull String fileName) {
-                        return fileName.endsWith(".jsonnet.TEMPLATE");
-                    }
-
-                    @NotNull
-                    @Override
-                    public String getPresentableString() {
-                        return ".jsonnet.TEMPLATE";
-                    }
-                }
-        );
+        fileTypeConsumer.consume(JsonnetFileType.INSTANCE, "jsonnet;libsonnet");
     }
 }
