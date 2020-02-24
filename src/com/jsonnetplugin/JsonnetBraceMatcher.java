eed3186adc6a90b50beede01f6d5a65b97b28da2
Li Haoyi <haoyi.li@databricks.com>
2020-02-23 19:34:18 -0800

    Add a trivial PairedBraceMatcher to allow simple brace/bracket/paren highlighting.
    Not entirely sure what the `structural` boolean argument to `BracePair` does, but for now I just copy what the intellij-scala folks do in https://github.com/JetBrains/intellij-scala/blob/idea201.x/scala/scala-impl/src/org/jetbrains/plugins/scala/highlighter/ScalaBraceMatcher.java and only set it to `true` for curly braces
    
    For now I just leave `isPairedBracesAllowedBeforeType` hardcoded to `false`, since it'll take more effort to come up with a proper heuristic that doesn't get too annoying. Empirically IntelliJ seems to still auto-pair braces/brackets/parens when opened at the end of the file, which seems like a reasonable start.

---
+++ src/com/jsonnetplugin/JsonnetBraceMatcher.java
@@ -0,0 +1,30 @@
+package com.jsonnetplugin;
+
+import com.intellij.lang.BracePair;
+import com.intellij.lang.PairedBraceMatcher;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.tree.IElementType;
+import com.jsonnetplugin.psi.JsonnetTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class JsonnetBraceMatcher implements PairedBraceMatcher {
+
+    private static final BracePair[] PAIRS = new BracePair[]{
+            new BracePair(JsonnetTypes.L_BRACKET, JsonnetTypes.R_BRACKET, false),
+            new BracePair(JsonnetTypes.L_PAREN, JsonnetTypes.R_PAREN, false),
+            new BracePair(JsonnetTypes.L_CURLY, JsonnetTypes.R_CURLY, true),
+    };
+
+    public BracePair[] getPairs() {
+        return PAIRS;
+    }
+
+    public boolean isPairedBracesAllowedBeforeType(@NotNull IElementType type, @Nullable IElementType tokenType) {
+        return false;
+    }
+
+    public int getCodeConstructStart(PsiFile file, int openingBraceOffset) {
+        return openingBraceOffset;
+    }
+}
