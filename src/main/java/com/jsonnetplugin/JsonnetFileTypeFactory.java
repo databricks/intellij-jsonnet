7100c34dd7b50fb9fe3ba6fa3dbe5ca27ec0d3ef
Santiago Castro <sacastro@umich.edu>
2020-03-04 22:23:58 -0500

    Set the 'since' to '2019.1'

---
+++ src/main/java/com/jsonnetplugin/JsonnetFileTypeFactory.java
@@ -0,0 +1,23 @@
+package com.jsonnetplugin;
+
+import com.intellij.openapi.fileTypes.*;
+import org.jetbrains.annotations.NotNull;
+
+public class JsonnetFileTypeFactory extends FileTypeFactory {
+    @Override
+    public void createFileTypes(@NotNull FileTypeConsumer fileTypeConsumer) {
+        fileTypeConsumer.consume(JsonnetFileType.INSTANCE, "jsonnet;libsonnet");
+        fileTypeConsumer.consume(JsonnetFileType.INSTANCE, new FileNameMatcher() {
+            @Override
+            public boolean accept(@NotNull String fileName) {
+                return fileName.endsWith(".jsonnet.TEMPLATE");
+            }
+
+            @NotNull
+            @Override
+            public String getPresentableString() {
+                return ".jsonnet.TEMPLATE";
+            }
+        });
+    }
+}
